cc7985f64b02f2897d6cafd5f21bbdb93079122f
Reza office <rzw.mhz82@gmail.com>
2025-04-26 13:33:57 +0330

    add project-print

---
+++ Band Name Generator.py
@@ -0,0 +1,10 @@
+print("welcome tO the band name generator")
+
+city = input("which city did you grow up in?\n")
+
+pet_name = input("whats your pet name?\n")
+
+print("your bane name could be :" + city + " " + pet_name)
+
+
+
---
+++ Python print Function.py
@@ -0,0 +1,2 @@
+print("Day 1 - Pythone Print Function")
+print("The Function is declared like this:\n print('what to print')")
